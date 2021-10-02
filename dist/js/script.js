78697d774f3159bf90fbca57ed8c42c1d8938fd8
Ajayi Samuel <71408523+sammiesleek@users.noreply.github.com>
2021-10-02 01:12:46 +0100

    update script.js

--- dist/js/script.js
+++ dist/js/script.js
@@ -18,3 +18,26 @@ function toggleMenu() {
     }
 
 }
+ /* smooth scroll */
+
+const sections = document.querySelectorAll('.sect')
+sections.forEach(sect => {
+    console.log(sect.offsetTop)
+    console.log(sect.offsetHeight)
+    window.pageXOffset=1000;
+});
+ function scrollActive(){
+     const scrollY = window.pageYOffset
+  
+     sections.forEach(current =>{
+         const sectionHeight = current.offsetHeight
+         const sectionTop = current.offsetTop - 150;
+         sectionId = current.getAttribute('id')
+         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav-links a[href*=' + sectionId + ']').classList.add('active')
+         }else{
+           document.querySelector('.nav-links a[href*=' + sectionId + ']').classList.remove('active')
+       }
+     })
+ }
+ window.addEventListener('scroll', scrollActive)
