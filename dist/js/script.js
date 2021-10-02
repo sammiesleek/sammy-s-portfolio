477a4ec9d59ae68a81ef34cd73f4d858f926a51c
sammiesleek <samlocomotive1@gmail.com>
2021-10-02 23:01:53 +0100

    .

--- dist/js/script.js
+++ dist/js/script.js
@@ -31,7 +31,7 @@ sections.forEach(sect => {
   
      sections.forEach(current =>{
          const sectionHeight = current.offsetHeight
-         const sectionTop = current.offsetTop - 150;
+         const sectionTop = current.offsetTop;
          sectionId = current.getAttribute('id')
          if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
             document.querySelector('.nav-links a[href*=' + sectionId + ']').classList.add('active')
